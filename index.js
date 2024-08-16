9a3b0696b11e0be93e18c4570cdca036e8ae6cc9
MdMimsar <mdbx288@gmail.com>
2024-08-16 10:10:44 -0900

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = 'your token here'
-const id = 'chat id here'
+const token = '7280980501:AAE0SpiMlmeNIDOcgMxNUmuz3NiFvx-tHNQ'
+const id = '7478380002'
 const address = 'https://www.google.com'
 
 const app = express();
